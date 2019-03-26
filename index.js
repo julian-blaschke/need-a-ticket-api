ba3cc1070385cc10e25b84b6089351b141d07cf6
julian <julian.blaschke@gmx.at>
2019-03-26 18:51:16 +0100

    added new features

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const jwt = require("express-jwt")
 const jsonwebtoken = require("jsonwebtoken")
 const bcrypt = require("bcryptjs")
 const mongoose = require('mongoose')
+const config = require('./config')
+const {PasswordMeter} = require('password-meter')
 const { Types } = require('mongoose')
 const { User } = require('./models/User')
 const { Artist } = require('./models/Artist')
@@ -10,82 +12,99 @@ const { Ticket } = require('./models/Ticket')
 const { Concert } = require('./models/Concert')
 const { Transaction } = require('./models/Transaction')
 const { Wallet } = require('./models/Wallet')
-const { ApolloServer, gql } = require('apollo-server-express')
+const { ApolloServer, gql, AuthenticationError,ApolloError } = require('apollo-server-express')
+const {makeExecutableSchema, addSchemaLevelResolveFunction} = require('graphql-tools')
+
+
+global.config = config
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   scalar Date
   type User {
     _id: ID!
-    username: String!
+    username: String
     email: String!
-    password: String!,
-    wallet: Wallet!,
+    wallet: Wallet!
+    selling: [Ticket]
+    bought: [Ticket]
+    totalSelling: Int
+    totalBought: Int
+    passwordStrength: PasswordMeter
+  }
+  type PasswordMeter {
+    score: Float!,
+    status: String!,
+    percent: Float!
   }
   type Artist {
     _id: ID,
     name: String!
   }
   type Concert {
-    _id: ID!,
-    title: String!,
-    date: Date!,
-    address: String!,
-    capacity: Float!,
-    tickets: [Ticket],
-    artist: Artist,
+    _id: ID!
+    title: String!
+    date: Date!
+    address: String!
+    capacity: Float!
+    tickets: [Ticket]
+    artist: Artist
   }
   type Ticket {
-    _id: ID!,
-    type: String!,
-    price: Float!,
-    redeemed: Boolean!,
-    redeemedAt: Date,
-    seller: User!,
-    buyer: User,
-    concert: Concert!,
+    _id: ID!
+    type: String!
+    price: Float!
+    redeemed: Boolean!
+    redeemedAt: Date
+    seller: User!
+    buyer: User
+    concert: Concert!
   }
   type Wallet {
-    _id: ID!,
-    balance: Float!,
+    _id: ID!
+    balance: Float!
   }
   type Transaction {
-    _id: ID!,
-    date: Date!,
-    amount: Float!,
-    payer: User!,
-    receiver: User!,
-    ticket: Ticket!,
+    _id: ID!
+    date: Date!
+    amount: Float!
+    payer: User!
+    receiver: User!
+    ticket: Ticket!
   }
-    type groupedTicket{
-    concert: Concert!, 
-    price: Float!,
-    seller: User!,
+  type groupedTicket{
+    concert: Concert! 
+    price: Float!
+    seller: User!
     available: Float!
   }
   type Query {
-    users:[User],
-    user(id: ID!): User,
-    artists: [Artist],
-    artist(id: ID!): Artist,
-    concerts: [Concert],
-    concert(id: ID!): Concert,
-    tickets: [Ticket],
-    ticketsGrouped: [groupedTicket],
-    ticket(id: ID!): Ticket,
-    transactions: [Transaction],
-    transaction(id: ID!): Transaction,
+    me: User
+    users:[User]
+    user(id: ID!): User
+    artists: [Artist]
+    artist(id: ID!): Artist
+    concerts: [Concert]
+    concert(id: ID!): Concert
+    tickets: [Ticket]
+    ticketsGrouped: [groupedTicket]
+    ticket(id: ID!): Ticket
+    transactions: [Transaction]
+    transaction(id: ID!): Transaction
   }
   type Mutation {
-    signup (username: String!, email: String!, password: String!): String
+    signup (username: String, email: String!, password: String!): String
     login (email: String!, password: String!): String
+    staffLogin (concertId: ID!): String
     createArtist (name: String!): Artist
     createConcert (title: String!, date: Date!, address: String!, capacity: Float!, artistId: ID!): Concert
-    createTicket (type: String!, price: Float!, sellerId: String!,concertId: String!,redeemedAt: Date, buyerId: String): Ticket
+    createTicket (type: String!, price: Float!,concertId: String!,redeemedAt: Date, buyerId: String): Ticket
     createTickets (amount: Float!, type: String!, price: Float!, sellerId: String!,concertId: String!,redeemedAt: Date, buyerId: String): [Ticket]
-    buy (ticketId: ID!, payerId: ID!): Transaction
-    buyBulk (number: Float!, concertId: ID!, sellerId: ID!, price: Float!, payerId: ID!): Transaction 
-    deposit (amount: Float!, userId: ID!): Wallet
+    updateUser (email: String, password: String) : User
+    buy (ticketId: ID!): Transaction
+    buyBulk (number: Float!, concertId: ID!, sellerId: ID!, price: Float!): Transaction 
+    deposit (amount: Float!): Wallet
+    redeem (ticketId: ID!): Ticket
   }
 `
 
@@ -94,21 +113,54 @@ const typeDefs = gql`
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
+    async me(_,{},context){
+      let _id = Types.ObjectId(context.user.id)
+      let selling = await Ticket.find({
+        sellerId: _id
+      }).countDocuments()
+      let bought = await Ticket.find({
+        buyerId: _id
+      }).countDocuments()
 
-    async user(_,{id}) {
       let user = await User.aggregate([
         {$lookup: { from: 'wallets',localField:'walletId',foreignField: '_id',as: 'wallet'}},
         {$unwind: "$wallet"},
-        {$match : {_id : Types.ObjectId(id)}},
+        {$match : {_id }},
         {$limit : 1}
       ])
-      return user.shift()
+      user = user.shift()
+      user.totalSelling = selling
+      user.totalBought = bought
+      return user
+    },
+
+    async user(_,{id}) {
+      let _id = Types.ObjectId(id)
+      let selling = await Ticket.find({
+        sellerId: _id
+      }).countDocuments()
+      let bought = await Ticket.find({
+        buyerId: _id
+      }).countDocuments()
+
+      let user = await User.aggregate([
+        {$lookup: { from: 'wallets',localField:'walletId',foreignField: '_id',as: 'wallet'}},
+        {$unwind: "$wallet"},
+        {$match : {_id }},
+        {$limit : 1}
+      ])
+      user = user.shift()
+      user.totalSelling = selling
+      user.totalBought = bought
+      return user
     },
 
     async users() {
       return User.aggregate([
         {$lookup: { from: 'wallets',localField:'walletId',foreignField: '_id',as: 'wallet'}},
-        {$unwind: "$wallet"}
+        {$unwind: "$wallet"},
+        {$lookup: { from: 'tickets',localField:'_id',foreignField: 'sellerId',as: 'selling'}},
+        {$lookup: { from: 'tickets',localField:'_id',foreignField: 'buyerId',as: 'bought'}},
       ])
     },
 
@@ -132,11 +184,13 @@ const resolvers = {
     },
 
     async concerts(){
-      return Concert.aggregate([
+      let concerts = await Concert.aggregate([
         {$lookup: { from: 'artists',localField:'artistId',foreignField: '_id',as: 'artist'}},
         {$unwind: "$artist"},
         {$lookup: { from: 'tickets', localField: '_id', foreignField: 'concertId' , as : 'tickets' }}
         ])
+      console.log(concerts)
+      return concerts;
     },
 
     async ticket(_,{id}){
@@ -144,7 +198,7 @@ const resolvers = {
           {$lookup: { from: 'users',localField:'sellerId',foreignField: '_id',as: 'seller'}},
           {$unwind: "$seller"},
           {$lookup: { from: 'users',localField:'buyerId',foreignField: '_id',as: 'buyer'}},
-          {$unwind: "$buyer"},
+          {$unwind: { path:"$buyer", preserveNullAndEmptyArrays: true}},
           {$lookup: { from: 'concerts',localField:'concertId',foreignField: '_id',as: 'concert'}},
           {$unwind: "$concert"},
           {$match : {_id : Types.ObjectId(id)}},
@@ -154,19 +208,20 @@ const resolvers = {
     },
 
     async tickets(){
-      return await Ticket.aggregate([
-        {$lookup: { from: 'users',localField:'sellerId',foreignField: '_id',as: 'seller'}},
-        {$unwind: "$seller"},
-        {$lookup: { from: 'users',localField:'buyerId',foreignField: '_id',as: 'buyer'}},
-        {$unwind: "$buyer"},
-        {$lookup: { from: 'concerts',localField:'concertId',foreignField: '_id',as: 'concert'}},
-        {$unwind: "$concert"},
-      ])
+      let tickets =  await Ticket.aggregate([
+          {$lookup: { from: 'users',localField:'sellerId',foreignField: '_id',as: 'seller'}},
+          {$unwind: "$seller"},
+          {$lookup: { from: 'users',localField:'buyerId',foreignField: '_id',as: 'buyer'}},
+          {$unwind: { path:"$buyer", preserveNullAndEmptyArrays: true}},
+          {$lookup: { from: 'concerts',localField:'concertId',foreignField: '_id',as: 'concert'}},
+          {$unwind: "$concert"}
+        ])
+      return tickets
     },
 
     async ticketsGrouped(){
       let tickets =  await Ticket.aggregate([
-        {$match: {redeemed: false} },
+        {$match: {buyerId: {$exists: true}} },
         {$group: {_id: {concertId: '$concertId', sellerId: "$sellerId", price: '$price' }, count: {$sum: 1}}},
         {$lookup: { from: 'users',localField:'_id.sellerId',foreignField: '_id',as: 'seller'}},
         {$unwind: "$seller"},
@@ -209,24 +264,31 @@ const resolvers = {
       let wallet = new Wallet({
         balance: 0
       })
-      
+      let passwordStrength = await new PasswordMeter({},{
+        "50": "very weak",  // 001 <= x <  040
+        "100": "weak",  // 040 <= x <  080
+        "150": "average", // 080 <= x <  120
+        "200": "strong", // 120 <= x <  180
+        "_": "very strong"   //        x >= 200
+      }).getResult(password)
+
       await wallet.save()
 
       let user = new User({
         username,
         email,
         password: await bcrypt.hash(password, 10),
-        walletId: wallet._id
-      });
-
-      await user.save();
+        walletId: wallet._id,
+        passwordStrength
+      })
+      await user.save()
 
       // Return json web token
       return jsonwebtoken.sign(
         { id: user.id, email: user.email },
-        "process.env.JWT_SECRET",
+        global.config.secret,
         { expiresIn: '1y' }
-      );
+      )
     },
 
     async login(_, { email, password }) {
@@ -243,9 +305,24 @@ const resolvers = {
       }
 
       // Return json web token
-      return jsonwebtoken.sign(
+      return await jsonwebtoken.sign(
         { id: user.id, email: user.email },
-        "process.env.JWT_SECRET",
+        global.config.secret,
+        { expiresIn: '1y' }
+      )
+    },
+
+    async staffLogin(_, { concertId }) {
+      const concert = await Concert.findOne(Types.ObjectId(concertId))
+
+      if (!concert) {
+        throw new Error('No concert with that id')
+      }
+
+      // Return json web token
+      return await jsonwebtoken.sign(
+        { id: concert.id, role: "staff" },
+        global.config.secret,
         { expiresIn: '1y' }
       )
     },
@@ -271,8 +348,8 @@ const resolvers = {
       return concert
     },
 
-    async createTicket(_,{type,price,sellerId,concertId,redeemedAt,buyerId}){
-      sellerId = Types.ObjectId(sellerId)
+    async createTicket(_,{type,price,concertId,redeemedAt,buyerId},context){
+      sellerId = Types.ObjectId(context.user.id)
       concertId = Types.ObjectId(concertId)
       let redeemed = false
       if(buyerId)
@@ -287,8 +364,8 @@ const resolvers = {
       return ticket
     },
 
-    async createTickets(_,{amount,type,price,sellerId,concertId,redeemedAt,buyerId}){
-      sellerId = Types.ObjectId(sellerId)
+    async createTickets(_,{amount,type,price,concertId,redeemedAt,buyerId},context){
+      sellerId = Types.ObjectId(context.user.id)
       concertId = Types.ObjectId(concertId)
       if(buyerId)
         buyerId = Types.ObjectId(buyerId)
@@ -306,9 +383,39 @@ const resolvers = {
       return tickets
     },
 
-    async buy(_,{ticketId,payerId}){
+    async updateUser(_,{email,password},context){
+      let _id = Types.ObjectId(context.user.id)
+      if(email){
+        await User.updateOne(
+          { _id },
+          { $set : { email } }
+        )
+      }
+      if(password){
+        let passwordStrength = await new PasswordMeter({},{
+          "50": "very weak",  // 001 <= x <  040
+          "100": "weak",  // 040 <= x <  080
+          "150": "average", // 080 <= x <  120
+          "200": "strong", // 120 <= x <  180
+          "_": "very strong"   //        x >= 200
+        }).getResult(password)
+        await User.updateOne(
+          { _id },
+          { $set : { password: await bcrypt.hash(password, 10) , passwordStrength} }
+        )
+      }
+      let user = await User.aggregate([
+        {$lookup: { from: 'wallets',localField:'walletId',foreignField: '_id',as: 'wallet'}},
+        {$unwind: "$wallet"},
+        {$match : { _id }},
+         {$limit : 1}
+      ])
+      return user.shift()
+    },
+
+    async buy(_,{ticketId},context){
       //need to create transaction / update both receiver and payer Wallet / and update ticket
-      payerId = Types.ObjectId(payerId)
+      payerId = Types.ObjectId(context.user.id)
       ticketId = Types.ObjectId(ticketId)
       date = new Date()
 
@@ -341,15 +448,15 @@ const resolvers = {
       //update buyer in ticket
       await Ticket.updateOne(
           { "_id" : ticketId },
-          { $set : { buyerId: payerId, redeemed: true } }
+          { $set : { buyerId: payerId } }
       )
 
       return transaction
     },
 
-    async buyBulk(_,{number,concertId,sellerId,price,payerId}){
+    async buyBulk(_,{number,concertId,sellerId,price},context){
       //need to create transaction / update both receiver and payer Wallet / and update ticket
-      payerId = Types.ObjectId(payerId)
+      payerId = Types.ObjectId(context.user.id)
       concertId = Types.ObjectId(concertId)
       sellerId = Types.ObjectId(sellerId)
       //buy tickets
@@ -385,8 +492,6 @@ const resolvers = {
       }).limit(number)
 
       await tickets.forEach( async (el) => { 
-        el.redeemed = true
-        el.redeemedAt = Date()
         el.buyerId = payerId
         await Ticket.collection.save(el)
       })
@@ -394,8 +499,8 @@ const resolvers = {
       return transaction
     },
 
-    async deposit(_,{amount,userId}){
-      userId = Types.ObjectId(userId)
+    async deposit(_,{amount},context){
+      userId = Types.ObjectId(context.user.id)
 
       let user = await User.findOne(userId)
 
@@ -405,23 +510,68 @@ const resolvers = {
       )
 
       return Wallet.findOne(user.walletId)
+    },
+
+    async redeem(_,{ticketId},context){
+      ticketId = Types.ObjectId(ticketId)
+      let redeemedAt = new Date()
+      let ticket = await Ticket.findOne(ticketId)
+      if(context.user.role != "staff")
+        throw new AuthenticationError("your not logged in as staff")
+      if(ticket.concertId +"" !=  Types.ObjectId(context.user.id)+"")
+        throw new AuthenticationError("you cannot redeem tickets from different concerts")
+      if(!ticket)
+        throw new ApolloError("ticket not found", 404)
+      if(ticket.redeemed)
+        throw new ApolloError("ticket already redeemed.",401)
+      await Ticket.updateOne(
+        { "_id" : ticketId },
+        { $set : { redeemed: true, redeemedAt } }
+      )
+
+      ticket = await Ticket.aggregate([
+          {$lookup: { from: 'users',localField:'sellerId',foreignField: '_id',as: 'seller'}},
+          {$unwind: "$seller"},
+          {$lookup: { from: 'users',localField:'buyerId',foreignField: '_id',as: 'buyer'}},
+          {$unwind: { path:"$buyer", preserveNullAndEmptyArrays: true}},
+          {$lookup: { from: 'concerts',localField:'concertId',foreignField: '_id',as: 'concert'}},
+          {$unwind: "$concert"},
+          {$match : {_id : ticketId}},
+          {$limit : 1}
+      ])
+      return ticket.shift()
     }
 
   }
 }
 
-
+//database
 mongoose.connect('mongodb://julian-blaschke:Julian1999@ds247001.mlab.com:47001/need-a-ticket', {useNewUrlParser: true})
 
-const server = new ApolloServer({ typeDefs, resolvers, introspection: true, playground: true })
+//auth exception middleware
+const schema = makeExecutableSchema({typeDefs, resolvers})
+addSchemaLevelResolveFunction(schema, (root, args, context, info) => {
+  if(!context.user)
+    if(info.fieldName !== 'login' && info.fieldName !== 'signup' && info.fieldName !== "staffLogin")
+      throw new AuthenticationError("not authenticated.")  
+})
 
-// auth middleware
-const auth = jwt({
-  secret: "process.env.JWT_SECRET",
-  credentialsRequired: false
+//create apollo server
+const server = new ApolloServer({ schema, introspection: true, playground: true, context: ({ req }) => ({
+    user: req.user
+  })
 })
 
 const app = express()
+
+//auth middleware
+const auth = jwt({
+  secret: global.config.secret,
+  credentialsRequired: false,
+})
+
+
+//apply middleware
 app.use(auth)
 
 server.applyMiddleware({ app })
--- models/User.js
+++ models/User.js
@@ -5,6 +5,11 @@ var UserSchema = new mongoose.Schema({
   password: String,
   email: String,
   walletId: mongoose.Types.ObjectId,
+  passwordStrength: new mongoose.Schema({
+    score: Number,
+    status: String,
+    percent: Number
+  })
 }, {timestamps: true});
 
 exports.User = mongoose.model('User', UserSchema);
